5013e10b5175dce363df18e051e69be303bb68dc
CARLOS MARIO OSPINA HIGUITA <carlosospina335@gmail.com>
2023-09-27 17:35:00 -0500

    Commit Inicial, Creacion Clase "Participantes"

---
+++ GestionEventos.API/Participantes.cs
@@ -0,0 +1,21 @@
+﻿namespace GestionEventos.API
+{
+    public class Participantes
+    {
+        // Atributos
+        public string Nombre { get; set; }
+        public string AfiliacionInstitucional { get; set; }
+        public string AreaDeInteres { get; set; }
+        public string TipoDeParticipacion { get; set; }
+
+        // Constructor
+        public Participantes(string nombre, string afiliacion, string area, string tipo)
+        {
+            Nombre = nombre;
+            AfiliacionInstitucional = afiliacion;
+            AreaDeInteres = area;
+            TipoDeParticipacion = tipo;
+        }
+
+    }
+}
